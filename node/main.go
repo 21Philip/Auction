d049284b2568856442c039b75b80b5eabe28dcae
Philip Rosenhagen <phro@itu.dk>
2024-11-20 19:18:40 +0100

    setup

--- node/main.go
+++ node/main.go
@@ -1,12 +1,7 @@
 package node
 
 import (
-	"fmt"
-	"net"
-
-	pb "/grpc"
-
-	"google.golang.org/grpc"
+	pb "github.com/21Philip/Auction/grpc"
 )
 
 type Node struct {
@@ -18,21 +13,5 @@ func main() {
 }
 
 func (n *Node) start() {
-	grpcServer := grpc.NewServer()
-	listener, err := net.Listen("tcp", n.address)
-	if err != nil {
-		fmt.Printf("Unable to start connection to server: %v\n", err)
-	}
-	fmt.Printf("server listening at %v\n", listener.Addr())
 
-	pb.RegisterNodeServer(grpcServer, n)
-	if err := n.registerService(); err != nil {
-		fmt.Printf("Node %d failed to register\n", n.id)
-	}
-
-	go n.nodeLogic(grpcServer)
-
-	if grpcServer.Serve(listener) != nil {
-		fmt.Printf("failed to serve: %v\n", err)
-	}
 }
