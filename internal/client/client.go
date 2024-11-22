4d3bba51f2877d1be1e2ced84d12144924a71322
Philip Rosenhagen <phro@itu.dk>
2024-11-22 23:54:23 +0100

    rough

--- internal/client/client.go
+++ internal/client/client.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -36,21 +38,83 @@ func (c *Client) StartClient() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
-		input := scanner.Text()
+		input := strings.Split(scanner.Text(), " ")
 		c.mu.Lock()
 
-		if input == "test" {
-			c.testCall()
+		if input[0] == "status" {
+			c.status()
 		}
 
-		if input == "quit" {
+		if input[0] == "bid" {
+			c.bid(input)
+		}
+
+		if input[0] == "quit" {
 			break
+		}
+
+		if input[0] == "test" {
+			c.testCall()
 		}
 
 		c.mu.Unlock()
 	}
 
 	fmt.Println("Client stopped!")
+}
+
+func (c *Client) status() {
+	curNode := c.network.Nodes[c.nodeId]
+	if curNode == nil {
+		fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	req := &pb.Empty{}
+	defer cancel()
+
+	reply, err := curNode.Result(ctx, req)
+	if err != nil {
+		c.changeNode(func() { c.status() })
+		return
+	}
+
+	fmt.Printf("Current winner: Client %d, bid %d\n", reply.Winner, reply.BidAmount)
+}
+
+func (c *Client) bid(input []string) {
+	curNode := c.network.Nodes[c.nodeId]
+	if curNode == nil {
+		fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
+		return
+	}
+
+	if len(input) != 2 {
+		fmt.Printf("CLIENT (you): Incorrect arguments to place bid. Correct use 'bid <amount>'")
+		return
+	}
+
+	bidAmount, err := strconv.Atoi(input[1])
+	if err != nil {
+		fmt.Printf("CLIENT (you): Cannot convert %s to int. Correct use 'bid <amount>'", input[1])
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	req := &pb.Amount{
+		Bidder: int32(c.id),
+		Amount: int32(bidAmount),
+	}
+	defer cancel()
+
+	reply, err := curNode.Bid(ctx, req)
+	if err != nil {
+		c.changeNode(func() { c.bid(input) })
+		return
+	}
+
+	fmt.Printf("%v\n", reply.Success)
 }
 
 func (c *Client) testCall() {
@@ -66,7 +130,7 @@ func (c *Client) testCall() {
 
 	reply, err := curNode.TestCall(ctx, req)
 	if err != nil {
-		c.changeNode(c.testCall)
+		c.changeNode(func() { c.testCall() })
 		return
 	}
 
