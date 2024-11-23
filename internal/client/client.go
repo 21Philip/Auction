26933e3371fffc2de16aa62639feaccec5a53252
Philip Rosenhagen <phro@itu.dk>
2024-11-23 01:22:10 +0100

    generic functions

--- internal/client/client.go
+++ internal/client/client.go
@@ -5,13 +5,13 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
 
 	pb "github.com/21Philip/Auction/internal/grpc"
 	nwPkg "github.com/21Philip/Auction/internal/network"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -63,83 +63,88 @@ func (c *Client) StartClient() {
 	fmt.Println("Client stopped!")
 }
 
-func (c *Client) status() {
+func makeCall[In any, Out any](c *Client, call func(pb.NodeClient, context.Context, In, ...grpc.CallOption) (Out, error), req In) Out {
+	var reply Out
+
 	curNode := c.network.Nodes[c.nodeId]
 	if curNode == nil {
 		fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
-		return
+		return reply
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	req := &pb.Empty{}
 	defer cancel()
 
-	reply, err := curNode.Result(ctx, req)
+	reply, err := call(curNode, ctx, req)
 	if err != nil {
-		c.changeNode(func() { c.status() })
-		return
+		fmt.Printf("CLIENT (you): Request to current node timed out. Establishing new connection\n")
+		c.nodeId++
+		return makeCall(c, call, req)
 	}
 
-	fmt.Printf("Current winner: Client %d, bid %d\n", reply.Winner, reply.BidAmount)
+	return reply
+}
+
+func (c *Client) status() {
+	/*
+		curNode := c.network.Nodes[c.nodeId]
+		if curNode == nil {
+			fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		req := &pb.Empty{}
+		defer cancel()
+
+		reply, err := curNode.Result(ctx, req)
+		if err != nil {
+			c.changeNode(func() { c.status() })
+			return
+		}
+
+		fmt.Printf("Current winner: Client %d, bid %d\n", reply.Winner, reply.BidAmount)
+	*/
 }
 
 func (c *Client) bid(input []string) {
-	curNode := c.network.Nodes[c.nodeId]
-	if curNode == nil {
-		fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
-		return
-	}
+	/*
+		curNode := c.network.Nodes[c.nodeId]
+		if curNode == nil {
+			fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
+			return
+		}
 
-	if len(input) != 2 {
-		fmt.Printf("CLIENT (you): Incorrect arguments to place bid. Correct use 'bid <amount>'")
-		return
-	}
+		if len(input) != 2 {
+			fmt.Printf("CLIENT (you): Incorrect arguments to place bid. Correct use 'bid <amount>'")
+			return
+		}
 
-	bidAmount, err := strconv.Atoi(input[1])
-	if err != nil {
-		fmt.Printf("CLIENT (you): Cannot convert %s to int. Correct use 'bid <amount>'", input[1])
-		return
-	}
+		bidAmount, err := strconv.Atoi(input[1])
+		if err != nil {
+			fmt.Printf("CLIENT (you): Cannot convert %s to int. Correct use 'bid <amount>'", input[1])
+			return
+		}
 
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	req := &pb.Amount{
-		Bidder: int32(c.id),
-		Amount: int32(bidAmount),
-	}
-	defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		req := &pb.Amount{
+			Bidder: int32(c.id),
+			Amount: int32(bidAmount),
+		}
+		defer cancel()
 
-	reply, err := curNode.Bid(ctx, req)
-	if err != nil {
-		c.changeNode(func() { c.bid(input) })
-		return
-	}
+		reply, err := curNode.Bid(ctx, req)
+		if err != nil {
+			c.changeNode(func() { c.bid(input) })
+			return
+		}
 
-	fmt.Printf("%v\n", reply.Success)
+		fmt.Printf("%v\n", reply.Success)
+	*/
 }
 
 func (c *Client) testCall() {
-	curNode := c.network.Nodes[c.nodeId]
-	if curNode == nil {
-		fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	req := &pb.Empty{}
-	defer cancel()
-
-	reply, err := curNode.TestCall(ctx, req)
-	if err != nil {
-		c.changeNode(func() { c.testCall() })
-		return
-	}
-
+	reply := makeCall(c, pb.NodeClient.TestCall, req)
 	fmt.Printf("%s\n", reply.Payload)
-}
-
-// Very simple for now. Should consider better logic at some point
-func (c *Client) changeNode(retry func()) {
-	fmt.Printf("CLIENT (you): Request to current node timed out. Establishing new connection\n")
-	c.nodeId++
-	retry()
 }
