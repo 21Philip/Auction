a7eedd8221854bd6d6f79ec5c1b02cf76ee90298
Philip Rosenhagen <phro@itu.dk>
2024-11-23 01:35:57 +0100

    integration of makeCall()

--- internal/client/client.go
+++ internal/client/client.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -63,13 +64,13 @@ func (c *Client) StartClient() {
 	fmt.Println("Client stopped!")
 }
 
-func makeCall[In any, Out any](c *Client, call func(pb.NodeClient, context.Context, In, ...grpc.CallOption) (Out, error), req In) Out {
+func makeCall[In any, Out any](c *Client, call func(pb.NodeClient, context.Context, In, ...grpc.CallOption) (Out, error), req In) (Out, error) {
 	var reply Out
 
 	curNode := c.network.Nodes[c.nodeId]
 	if curNode == nil {
 		fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
-		return reply
+		return reply, fmt.Errorf("no more nodes")
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
@@ -82,69 +83,48 @@ func makeCall[In any, Out any](c *Client, call func(pb.NodeClient, context.Conte
 		return makeCall(c, call, req)
 	}
 
-	return reply
+	return reply, nil
 }
 
 func (c *Client) status() {
-	/*
-		curNode := c.network.Nodes[c.nodeId]
-		if curNode == nil {
-			fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
-			return
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), timeout)
-		req := &pb.Empty{}
-		defer cancel()
-
-		reply, err := curNode.Result(ctx, req)
-		if err != nil {
-			c.changeNode(func() { c.status() })
-			return
-		}
-
-		fmt.Printf("Current winner: Client %d, bid %d\n", reply.Winner, reply.BidAmount)
-	*/
+	req := &pb.Empty{}
+	reply, err := makeCall(c, pb.NodeClient.Result, req)
+	if err != nil {
+		return
+	}
+	fmt.Printf("Current winner: Client %d, bid %d\n", reply.Winner, reply.BidAmount)
 }
 
 func (c *Client) bid(input []string) {
-	/*
-		curNode := c.network.Nodes[c.nodeId]
-		if curNode == nil {
-			fmt.Printf("CLIENT (you): Seems all nodes are unavailable. Consider using 'quit' to exit program\n")
-			return
-		}
+	if len(input) != 2 {
+		fmt.Printf("CLIENT (you): Incorrect arguments to place bid. Correct use 'bid <amount>'")
+		return
+	}
 
-		if len(input) != 2 {
-			fmt.Printf("CLIENT (you): Incorrect arguments to place bid. Correct use 'bid <amount>'")
-			return
-		}
+	bidAmount, err := strconv.Atoi(input[1])
+	if err != nil {
+		fmt.Printf("CLIENT (you): Cannot convert %s to int. Correct use 'bid <amount>'", input[1])
+		return
+	}
 
-		bidAmount, err := strconv.Atoi(input[1])
-		if err != nil {
-			fmt.Printf("CLIENT (you): Cannot convert %s to int. Correct use 'bid <amount>'", input[1])
-			return
-		}
+	req := &pb.Amount{
+		Bidder: int32(c.id),
+		Amount: int32(bidAmount),
+	}
 
-		ctx, cancel := context.WithTimeout(context.Background(), timeout)
-		req := &pb.Amount{
-			Bidder: int32(c.id),
-			Amount: int32(bidAmount),
-		}
-		defer cancel()
+	reply, err := makeCall(c, pb.NodeClient.Bid, req)
+	if err != nil {
+		return
+	}
 
-		reply, err := curNode.Bid(ctx, req)
-		if err != nil {
-			c.changeNode(func() { c.bid(input) })
-			return
-		}
-
-		fmt.Printf("%v\n", reply.Success)
-	*/
+	fmt.Printf("%v\n", reply.Success)
 }
 
 func (c *Client) testCall() {
 	req := &pb.Empty{}
-	reply := makeCall(c, pb.NodeClient.TestCall, req)
+	reply, err := makeCall(c, pb.NodeClient.TestCall, req)
+	if err != nil {
+		return
+	}
 	fmt.Printf("%s\n", reply.Payload)
 }
